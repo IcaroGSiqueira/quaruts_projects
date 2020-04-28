b5e616e633173f899a3e2ef238d74a7cb57f04a0
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2020-04-28 18:06:32 -0300

    commit

---
+++ trabalho1/meally_abc/meally_abc.vhd
@@ -0,0 +1,71 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY meally_abc IS
+PORT (
+	clk, rst	: IN std_logic;
+   entrada	: IN std_logic_vector(1 DOWNTO 0);
+	saida		: OUT std_logic
+);
+END meally_abc;
+
+ARCHITECTURE arquitetura OF meally_abc IS
+
+type estados is (A, B, C);
+
+signal estado_atual, estado_proximo : estados;
+
+BEGIN
+
+PROCESS(clk, rst)
+
+BEGIN
+	IF (rst = '1') THEN
+		estado_atual <= A;
+	ELSIF (clk'EVENT AND clk = '1') THEN
+		estado_atual <= estado_proximo;
+	END IF;
+END PROCESS;
+
+PROCESS(estado_atual, entrada)
+
+BEGIN
+
+	CASE estado_atual IS
+		WHEN A =>
+			CASE entrada IS 
+				WHEN "00" =>
+					estado_proximo <= B;			
+					saida <= '0';
+				WHEN OTHERS =>
+					estado_proximo <= A;
+					saida <= '0';
+			END CASE;
+
+		WHEN B =>
+			CASE entrada IS 
+				WHEN "01" =>
+					estado_proximo <= C;
+					saida <= '0';
+				WHEN "00" =>
+					estado_proximo <= B;
+					saida <= '0';
+				WHEN OTHERS =>
+					estado_proximo <= A;
+					saida <= '0';
+			END CASE;
+
+		WHEN C =>
+			CASE entrada IS
+				WHEN "10" =>
+					estado_proximo <= A;
+					saida <= '1';
+				WHEN OTHERS =>
+					estado_proximo <= A;
+					saida <= '0';
+			END CASE;
+
+	END CASE;
+END PROCESS;
+
+END arquitetura;
---
+++ trabalho1/meally_abc/tb_meally_abc.vhd
@@ -0,0 +1,59 @@
+LIBRARY ieee;                                               
+USE ieee.std_logic_1164.all;                                
+
+ENTITY tb_meally_abc IS
+END tb_meally_abc;
+
+ARCHITECTURE testbench OF tb_meally_abc IS 
+
+	SIGNAL clk		: std_logic;
+	SIGNAL rst		: std_logic; 
+	SIGNAL entrada	: std_logic_vector(1 DOWNTO 0);
+	SIGNAL saida	: std_logic;
+	
+	COMPONENT meally_abc
+	PORT (
+		clk		: IN std_logic; 
+		rst		: IN std_logic;
+		entrada	: IN std_logic_vector(1 DOWNTO 0);
+		saida		: OUT std_logic
+	);
+	END COMPONENT;
+
+BEGIN 
+
+	i1: meally_abc
+	PORT MAP (
+		clk => clk,
+		rst => rst,
+		entrada => entrada,
+		saida => saida
+	);
+		
+	clock: PROCESS
+	BEGIN 
+		clk <= '0';
+		wait for 10 ns;
+		clk <= '1';
+		wait for 10 ns;
+	END PROCESS;
+				
+	reset: PROCESS
+	BEGIN
+		rst <= '1';
+		wait for 20 ns;
+		rst <= '0';
+		wait;  
+	END PROCESS;
+				
+	entry: PROCESS
+	BEGIN 
+		entrada <= "00";
+		wait for 20 ns;
+		entrada <= "01";
+		wait for 20 ns;
+		entrada <= "10";
+		wait for 20 ns;
+        
+	END PROCESS;
+END testbench;
